a25049de9ea27fa3f4de995e8106061c7efe15eb
vuduykhuong1412 <khuongvd.brainos@gmail.com>
2022-06-12 21:35:05 +0700

    Commit source code project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from ast import pattern
 from django.contrib import admin
 from django.urls import re_path
 from django.conf.urls import include
